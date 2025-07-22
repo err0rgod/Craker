3b917bdb522764c746153a0b04d9ff36fd14d4d1
err0rgod <ndgaming458@gmail.com>
2025-07-22 13:52:41 +0530

    func for loop wordlist

--- crack.py
+++ crack.py
@@ -1,4 +1,5 @@
 import argparse
+import hashlib
 
 
 
@@ -7,4 +8,24 @@ import argparse
 
 def main(hash,hash_type):
 
-    argparse.ArgumentParser
+    parser = argparse.ArgumentParser(description="Hash Cracker ")
+    
+    parser.add_argument("-hash","--hash_content",required=True,type=str,help="Enter the Hash to crack")
+    parser.add_argument("-type","--hash_type",required=True,type=str,help="Type of the Hash")
+    parser.add_argument("-w","--wordlist_path",type=str,help="Mention the path of the wordlist to brute force the passwords")
+    parser.add_argument("-t","--threads",type=int,default=30,help="Enter the Number of threads")
+    parser.add_argument("-s","--string",type=str,help="Enter the possible string content if possible")
+
+
+    args = parser.parse_args()
+
+    hash = args.hash_content
+    hash_type = args.hash_type
+    wordlist = args.wordlist
+    threads = args.threads
+
+
+
+
+
+    def 
