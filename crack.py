406c7e08789a5e612a073dac4ad07132057c086b
err0rgod <ndgaming458@gmail.com>
2025-07-22 13:57:55 +0530

    test wordlist featurte

--- crack.py
+++ crack.py
@@ -10,8 +10,8 @@ def main(hash,hash_type):
 
     parser = argparse.ArgumentParser(description="Hash Cracker ")
     
-    parser.add_argument("-hash","--hash_content",required=True,type=str,help="Enter the Hash to crack")
-    parser.add_argument("-type","--hash_type",required=True,type=str,help="Type of the Hash")
+    #parser.add_argument("-hash","--hash_content",required=True,type=str,help="Enter the Hash to crack")
+    #parser.add_argument("-type","--hash_type",required=True,type=str,help="Type of the Hash")
     parser.add_argument("-w","--wordlist_path",type=str,help="Mention the path of the wordlist to brute force the passwords")
     parser.add_argument("-t","--threads",type=int,default=30,help="Enter the Number of threads")
     parser.add_argument("-s","--string",type=str,help="Enter the possible string content if possible")
@@ -28,4 +28,11 @@ def main(hash,hash_type):
 
 
 
-    def 
+def words(wordlist):
+    with open(wordlist, 'r') as f:
+        return [line.strip('\n') for line in f]
+        
+
+
+
+print(words(wordlist))
